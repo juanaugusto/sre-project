fa15e8007471ae59957cf6c97918769fc29d2c87
Juan Augusto <juan.paula@corp.globo.com>
2020-03-21 10:40:45 -0300

    Create a job for get Tweets and insert them in MongoDB

---
+++ job/app.py
@@ -0,0 +1,54 @@
+import os
+import pymongo
+import time
+from clients.twitter.api import TwitterAPI
+
+
+BEARER_TOKEN = os.environ['BEARER_TOKEN']
+MONGO_ROOT_USERNAME = os.environ['MONGO_ROOT_USERNAME']
+MONGO_ROOT_PASSWORD = os.environ['MONGO_ROOT_PASSWORD']
+MONGO_HOST = os.environ['MONGO_HOST']
+HASHTAGS = os.environ['HASHTAGS']
+
+time.sleep(10) # Wait Mongo DB be ready
+
+mongo_client = pymongo.MongoClient('mongodb://%s:%s@%s:27017/admin' % 
+                                    (MONGO_ROOT_USERNAME, 
+                                     MONGO_ROOT_PASSWORD, 
+                                     MONGO_HOST))
+mongo_collection = mongo_client.twitterDB.tweets
+
+twitter_api = TwitterAPI(BEARER_TOKEN)
+
+hashtags_array = HASHTAGS.split(';')
+
+while True:
+
+    for hashtag in hashtags_array:
+        tweets = twitter_api.search_tweets(hashtag, 100)
+
+        for tweet in tweets:
+            tweet_id = tweet['id']
+            tweet_text = tweet['text']
+            tweet_created_at = tweet['created_at'] 
+            tweet_lang = tweet['lang']
+            user_id = tweet['user']['id']
+            user_name =  tweet['user']['name']
+            user_followers_count = tweet['user']['followers_count']
+            user_location = tweet['user']['location']
+
+            mongo_collection.insert_one({
+                'tweet_hashtag': hashtag,
+                'tweet_id': tweet_id,
+                'tweet_text': tweet_text,
+                'tweet_created_at': tweet_created_at,
+                'tweet_lang': tweet_lang,
+                'user_id': user_id,
+                'user_name': user_name,
+                'user_followers_count': user_followers_count,
+                'user_location': user_location
+            })
+
+        time.sleep(2) # Wait 2 seconds before hit Twitter API
+
+    time.sleep(600) # Wait 10 minutes before hit Twitter API again
