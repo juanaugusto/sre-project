48b2c6ddb3a7ae2461304c0b138204cd96e3219c
Juan Augusto <juan.paula@corp.globo.com>
2020-03-21 13:56:23 -0300

    Remove 3rd party bson and finish first question

--- api/app.py
+++ api/app.py
@@ -1,4 +1,21 @@
+import bson
+import os
+import pymongo
 from flask import Flask
+from flask import jsonify
+
+
+MONGO_ROOT_USERNAME = os.environ['MONGO_ROOT_USERNAME']
+MONGO_ROOT_PASSWORD = os.environ['MONGO_ROOT_PASSWORD']
+MONGO_HOST = os.environ['MONGO_HOST']
+
+mongo_client = pymongo.MongoClient('mongodb://%s:%s@%s:27017/admin' % 
+                                    (MONGO_ROOT_USERNAME, 
+                                     MONGO_ROOT_PASSWORD, 
+                                     MONGO_HOST))
+
+tweets_collection = mongo_client.twitterDB.tweets
+users_collection = mongo_client.twitterDB.users
 
 app = Flask(__name__)
 
@@ -7,7 +24,18 @@ def what_users_with_most_followers():
     # Quais são os 5 (cinco) usuários, da 
     # amostra coletada, que possuem mais
     # seguidores?
-    return "oi"
+    # db.getCollection('users').find().sort( { user_followers_count: -1 } ).limit(5)
+
+    users = users_collection.find() \
+                .sort([('user_followers_count', pymongo.DESCENDING)]) \
+                .limit(5)
+
+    users = [{'user_id': user['user_id'], 
+              'user_name': user['user_name'],
+              'user_followers_count': user['user_followers_count']} 
+              for user in users]
+
+    return jsonify({'users': users})
 
 @app.route("/total/tweets/hour/<int:year>/<int:month>/<int:day>")
 def total_tweets_per_hour(year, month, day):
@@ -16,8 +44,8 @@ def total_tweets_per_hour(year, month, day):
     # hashtag)?
     return "Hello, Wolddd!"
 
-@app.route("/total/tweets/hashtag/language/location")
-def total_tweets_per_hashtag_and_language_location():
+@app.route("/total/tweets/hashtag/language/location/<user_name>")
+def total_tweets_per_hashtag_and_language_location(user_name):
     # Qual o total de postagens
     # para cada uma das #tag por idioma/país do
     # usuário que postou;
