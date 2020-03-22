55ac7c4650c74aaafcc870b11cf9a7c5bc5e4ebd
Juan Augusto <juan.paula@corp.globo.com>
2020-03-22 12:43:47 -0300

    Add main entrance to job

--- job/app.py
+++ job/app.py
@@ -6,31 +6,31 @@ from clients.twitterapi import TwitterAPI
 from dateutil.parser import parse
 from util.util import insert_in_mongo_by_statuses
 
+if __name__ == '__main__':
+    BEARER_TOKEN = os.environ['BEARER_TOKEN']
+    MONGO_ROOT_USERNAME = os.environ['MONGO_ROOT_USERNAME']
+    MONGO_ROOT_PASSWORD = os.environ['MONGO_ROOT_PASSWORD']
+    MONGO_HOST = os.environ['MONGO_HOST']
+    HASHTAGS = os.environ['HASHTAGS']
 
-BEARER_TOKEN = os.environ['BEARER_TOKEN']
-MONGO_ROOT_USERNAME = os.environ['MONGO_ROOT_USERNAME']
-MONGO_ROOT_PASSWORD = os.environ['MONGO_ROOT_PASSWORD']
-MONGO_HOST = os.environ['MONGO_HOST']
-HASHTAGS = os.environ['HASHTAGS']
+    time.sleep(20) # Wait Mongo DB be ready
 
-time.sleep(20) # Wait Mongo DB be ready
+    mongo_client = pymongo.MongoClient('mongodb://%s:%s@%s:27017/admin' % 
+                                        (MONGO_ROOT_USERNAME, 
+                                        MONGO_ROOT_PASSWORD, 
+                                        MONGO_HOST))
 
-mongo_client = pymongo.MongoClient('mongodb://%s:%s@%s:27017/admin' % 
-                                    (MONGO_ROOT_USERNAME, 
-                                     MONGO_ROOT_PASSWORD, 
-                                     MONGO_HOST))
+    tweets_collection = mongo_client.twitterDB.tweets
+    users_collection = mongo_client.twitterDB.users
 
-tweets_collection = mongo_client.twitterDB.tweets
-users_collection = mongo_client.twitterDB.users
+    twitter_api = TwitterAPI(BEARER_TOKEN)
 
-twitter_api = TwitterAPI(BEARER_TOKEN)
+    hashtags_array = HASHTAGS.split(';')
 
-hashtags_array = HASHTAGS.split(';')
+    while True:
+        insert_in_mongo_by_statuses(hashtags_array,
+                                    twitter_api,
+                                    tweets_collection, 
+                                    users_collection)
 
-while True:
-    insert_in_mongo_by_statuses(hashtags_array,
-                                twitter_api,
-                                tweets_collection, 
-                                users_collection)
-
-    time.sleep(600) # Hit again the Twitter API in the next 10 minutes
+        time.sleep(600) # Hit again the Twitter API in the next 10 minutes
