5bff324919397b2efcb65743c901d20b5b1a111f
Juan Augusto <juan.paula@corp.globo.com>
2020-03-22 21:30:16 -0300

    Mock logging

--- api/test/test_api.py
+++ api/test/test_api.py
@@ -2,6 +2,7 @@
 from app import app
 from test.fixtures.fixtures import get_tweets
 from test.fixtures.fixtures import get_users
+from unittest.mock import patch
 import pymongo
 import os
 import unittest
@@ -35,11 +36,13 @@ class TestApi(unittest.TestCase):
     def tearDownClass(cls):
         cls.mongo_client.drop_database('twitterDB')
 
-    def test_it_gives_404_in_route_that_does_not_exists(cls):
+    @patch('app.logging')
+    def test_it_gives_404_in_route_that_does_not_exists(cls, mock):
         response = cls.api_app.get('/anything')
         cls.assertEqual(response.status_code, 404)
 
-    def test_what_users_with_most_followers(cls):
+    @patch('app.logging')
+    def test_what_users_with_most_followers(cls, mock):
         response = cls.api_app.get('/what/users/most/followers')
 
         with open('/app/test/fixtures/test_what_users_with_most_followers.json') as file_:
@@ -48,7 +51,8 @@ class TestApi(unittest.TestCase):
         cls.assertEqual(response.data.decode('utf-8'),
                         api_json_return)
 
-    def test_total_tweets_per_hour(cls):
+    @patch('app.logging')
+    def test_total_tweets_per_hour(cls, mock):
         response = cls.api_app.get('/total/tweets/hour')
         
         with open('/app/test/fixtures/test_total_tweets_per_hour.json') as file_:
@@ -57,7 +61,8 @@ class TestApi(unittest.TestCase):
         cls.assertEqual(response.data.decode('utf-8'),
                         api_json_return)
 
-    def test_total_tweets_per_hashtag_and_language_location_for_user_with_one_tweet(cls):
+    @patch('app.logging')
+    def test_total_tweets_per_hashtag_and_language_location_for_user_with_one_tweet(cls, mock):
         response = cls.api_app.get('/total/tweets/hashtag/language/location/14')
 
         with open('/app/test/fixtures/test_total_tweets_per_hashtag_and_language_location_for_user_with_one_tweet.json') as file_:
@@ -66,7 +71,8 @@ class TestApi(unittest.TestCase):
         cls.assertEqual(response.data.decode('utf-8'),
                         api_json_return)
 
-    def test_total_tweets_per_hashtag_and_language_location_for_user_with_many_tweets(cls):
+    @patch('app.logging')
+    def test_total_tweets_per_hashtag_and_language_location_for_user_with_many_tweets(cls, mock):
         response = cls.api_app.get('/total/tweets/hashtag/language/location/10')
 
         with open('/app/test/fixtures/test_total_tweets_per_hashtag_and_language_location_for_user_with_many_tweets.json') as file_:
@@ -75,7 +81,8 @@ class TestApi(unittest.TestCase):
         cls.assertEqual(response.data.decode('utf-8'),
                         api_json_return)
 
-    def test_total_tweets_per_hashtag_and_language_location_for_non_existent_user(cls):
+    @patch('app.logging')
+    def test_total_tweets_per_hashtag_and_language_location_for_non_existent_user(cls, mock):
         response = cls.api_app.get('/total/tweets/hashtag/language/location/100')
         cls.assertEqual(response.status_code, 500)
 
